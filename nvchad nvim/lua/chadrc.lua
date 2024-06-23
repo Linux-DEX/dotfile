47ac2dc75461e74f6d89ac127e60d45babdcfde1
Linux-DEX <sarabjeet.9353@gmail.com>
2024-06-23 14:34:20 +0530

    nvchad and tmux config

---
+++ nvchad nvim/lua/chadrc.lua
@@ -0,0 +1,33 @@
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "catppuccin",
+
+  statusline={
+    separator_style="round",
+  },
+
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
+
+  term = {
+    hl = "Normal:term,WinSeparator:WinSeparator",
+    sizes = { sp = 0.6, vsp = 0.5 },
+    float = {
+      relative = "editor",
+      row = 0.02,
+      col = 0.04,
+      width = 0.9,
+      height = 0.83,
+      border = "single",
+    }
+  }
+}
+
+return M
