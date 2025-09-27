9a0e18a853343fae46e04dc11e57defbc7e54766
Linux-DEX <sarabjeet.9353@gmail.com>
2025-09-27 06:41:08 +0530

    lsp modify gd keybinding

--- lazyvim/lua/linuxdex/plugins/lsp/lspconfig.lua
+++ lazyvim/lua/linuxdex/plugins/lsp/lspconfig.lua
@@ -10,6 +10,48 @@ return {
     -- import cmp-nvim-lsp plugin
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
+    -- Custom goto definition that opens in new tab using 'tab drop'
+    local function goto_definition_tabdrop()
+      local client = vim.lsp.get_clients({ bufnr = 0 })[1]
+      if not client then
+        vim.notify("No active LSP client", vim.log.levels.WARN)
+        return
+      end
+
+      local encoding = client.offset_encoding or "utf-16"
+      local params = vim.lsp.util.make_position_params(nil, encoding)
+
+      vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result, ctx, _)
+        if err then
+          vim.notify("Error when finding definition: " .. err.message, vim.log.levels.ERROR)
+          return
+        end
+        if not result or vim.tbl_isempty(result) then
+          vim.notify("No definition found", vim.log.levels.INFO)
+          return
+        end
+
+        -- handle multiple results
+        local def = result[1] or result
+        if def.uri == nil and def.targetUri then
+          def.uri = def.targetUri
+          def.range = def.targetSelectionRange
+        end
+
+        local filename = vim.uri_to_fname(def.uri)
+        if not filename or filename == "" then
+          vim.notify("No file found for definition", vim.log.levels.ERROR)
+          return
+        end
+
+        vim.cmd("tab drop " .. vim.fn.fnameescape(filename))
+        local row = (def.range.start.line or 0) + 1
+        local col = def.range.start.character or 0
+        vim.api.nvim_win_set_cursor(0, { row, col })
+      end)
+    end
+
+
     local keymap = vim.keymap -- for conciseness
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -25,11 +67,15 @@ return {
         opts.desc = "Go to declaration"
         keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
+        -- opts.desc = "Show LSP definitions"
+        -- keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
         opts.desc = "Show LSP definitions"
-        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+        -- keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+        keymap.set("n", "gd", goto_definition_tabdrop, vim.tbl_extend("force", opts, { desc = "Go to definition (tab drop)" }))
 
         opts.desc = "Show LSP implementations"
-        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+        -- keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+        keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, { desc = "Go to implementation" }))
 
         opts.desc = "Show LSP type definitions"
         keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
@@ -81,21 +127,21 @@ return {
       capabilities = capabilities,
     })
 
-    -- vim.lsp.config("svelte", {
-    --   on_attach = function(client, bufnr)
-    --     vim.api.nvim_create_autocmd("BufWritePost", {
-    --       pattern = { "*.js", "*.ts" },
-    --       callback = function(ctx)
-    --         -- Here use ctx.match instead of ctx.file
-    --         client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-    --       end,
-    --     })
-    --   end,
-    -- })
+    vim.lsp.config("svelte", {
+      on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePost", {
+          pattern = { "*.js", "*.ts" },
+          callback = function(ctx)
+            -- Here use ctx.match instead of ctx.file
+            client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+          end,
+        })
+      end,
+    })
 
-    -- vim.lsp.config("graphql", {
-    --   filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-    -- })
+    vim.lsp.config("graphql", {
+      filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+    })
 
     vim.lsp.config("emmet_ls", {
       filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
@@ -130,5 +176,27 @@ return {
         },
       },
     })
+
+    vim.lsp.config("pyright", {
+      before_init = function(_, config)
+        -- venv-selector will store your selected venv here:
+        local venv_python = vim.g.venv_selector_python_path
+        if venv_python and vim.fn.executable(venv_python) == 1 then
+          config.settings = config.settings or {}
+          config.settings.python = config.settings.python or {}
+          config.settings.python.pythonPath = venv_python
+        end
+      end,
+      settings = {
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            useLibraryCodeForTypes = true,
+            diagnosticMode = "workspace",
+          },
+        },
+      },
+    })
+
   end,
 }
